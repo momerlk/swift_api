d13c3859a76a90b15f810a0e39cf4d6da7352300
Omer Ali Malik <omeralimalik96@gmail.com>
2024-06-15 21:37:57 +0500

    user_interactions updated to support pandas

--- model/recommender/model.py
+++ model/recommender/model.py
@@ -1,10 +1,19 @@
 import json
 import pandas as pd
 
+# Define the path to your JSON file
 file = './user_interactions.json'
+
+# Open the JSON file and load it into a Python dictionary
 with open(file) as train_file:
     dict_train = json.load(train_file)
 
-# converting json dataset from dictionary to dataframe
+# Convert the dictionary to a pandas DataFrame
 train = pd.DataFrame.from_dict(dict_train, orient='index')
+
+# Reset the index to move the index (which is typically the keys from the JSON) to a column
 train.reset_index(level=0, inplace=True)
+
+
+# Display the DataFrame to verify
+print(train.head())
